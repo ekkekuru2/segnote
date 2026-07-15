77b2af1d255a848000629b708e1de00df191b4ee
ekkekuru2 <ekke@ekke.jp>
2026-07-15 21:08:16 +0900

    setup prisma

--- flake.nix
+++ flake.nix
@@ -34,11 +34,20 @@
             pnpm
             typescript
             typescript-language-server
+            prisma-engines
+            prisma
+            openssl
           ];
           shellHook = ''
             export SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt
             echo "uv version: $(uv --version)"
             echo "python version: $(python --version)"
+            export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig";
+            export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [ pkgs.openssl ]}:$LD_LIBRARY_PATH"
+            export PRISMA_SCHEMA_ENGINE_BINARY="${pkgs.prisma-engines}/bin/schema-engine"
+            export PRISMA_QUERY_ENGINE_BINARY="${pkgs.prisma-engines}/bin/query-engine"
+            export PRISMA_QUERY_ENGINE_LIBRARY="${pkgs.prisma-engines}/lib/libquery_engine.node"
+            export PRISMA_FMT_BINARY="${pkgs.prisma-engines}/bin/prisma-fmt"
           '';
         };
       });
